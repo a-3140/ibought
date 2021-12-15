6489fd83596a889842a312c372cb4dc4d336a797
a-3140 <aatienza.3140@gmail.com>
2021-12-15 21:38:39 +0800

    [tech]: Use Login instead of another component

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,55 @@
   <template>
-  <div>
-    <login-form />
-  </div>
+  <el-form ref="form" :model="form" @input.native="updateIsFormValidated">
+    <el-form-item
+      prop="email"
+      :rules="[{ required: true, message: 'please input your email' }]"
+    >
+      <el-input v-model="form.email" :maxlength="100" placeholder="email" />
+    </el-form-item>
+    <el-form-item
+      prop="password"
+      :rules="[{ required: true, message: 'please input your password' }]"
+    >
+      <el-input
+        type="password"
+        v-model="form.password"
+        :maxlength="100"
+        placeholder="password"
+      />
+    </el-form-item>
+    <el-button :disabled="!isFormValid" type="info" round> submit </el-button>
+  </el-form>
 </template>
 
 <script>
-import LoginForm from "@/components/Forms/LoginForm.vue";
+import { mapActions } from "vuex";
+
 export default {
   name: "Login",
-  components: {
-    LoginForm,
+  data() {
+    return {
+      isFormValid: false,
+      form: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    ...mapActions(["login"]),
+    loginUser: function () {
+      this.login(this.authDetails).then(() => this.$router.push("/wants"));
+    },
+    updateIsFormValidated() {
+      this.$refs.form.validate((isValid) => {
+        this.isFormValid = isValid;
+      });
+    },
   },
 };
 </script>
 <style scoped>
+.el-button {
+  width: 100%;
+}
 </style>
